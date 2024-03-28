d11afb44c070fc6659afe088edffe4a4f05bfc47
hreshnov <141960185+hreshnov@users.noreply.github.com>
2024-03-29 00:18:27 +0200

    without support

--- bot/keyboards/reply.py
+++ bot/keyboards/reply.py
@@ -8,9 +8,7 @@ keyboard_buttons = []
 phrases = load_menu()
 
 my_search = phrases['my_search']
-support_us = phrases['support_us']
 keyboard_buttons.append([KeyboardButton(text=my_search)])
-keyboard_buttons.append([KeyboardButton(text=support_us)])
 
 
 main_reply = ReplyKeyboardMarkup(keyboard=keyboard_buttons, resize_keyboard=True)
